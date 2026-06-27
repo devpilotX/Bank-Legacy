6a20546e84c071a2b756f2a35ea640d588b8ee13
devpilotx <devpilotx@gmail.com>
2026-06-28 00:18:06 +0530

    Add Spotless to tidy imports and whitespace, checked during verify

--- backend/src/main/java/com/corewise/modernization/security/SecurityConfig.java
+++ backend/src/main/java/com/corewise/modernization/security/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
